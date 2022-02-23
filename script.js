2095e4148edb45f2243a2c5794e1653155f66624
Junelle Puzon <nellepuzon@gmail.com>
2022-02-23 15:19:21 +0800

    background and greeting

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // current time
 
-const time = document.querySelector('.time');
+const time = document.querySelector(".time");
 
 function showTime() {
   let today = new Date(),
     hour = today.getHours(),
     min = today.getMinutes();
 
-    // period
-    const amPm = hour >= 12 ? "PM" : "AM";
+  // period
+  const amPm = hour >= 12 ? "PM" : "AM";
 
-    // format
-    hour = hour % 12 || 12;
+  // format
+  hour = hour % 12 || 12;
 
-    // output
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)} ${amPm}`;
+  // output
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)} ${amPm}`;
 }
 
 // add zero
@@ -23,3 +23,30 @@ function addZero(n) {
 }
 
 showTime();
+
+// background and greeting
+
+const greeting = document.getElementById("greeting");
+
+function backgroundGreeting() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    // morning
+    document.body.style.backgroundImage = "url('images/morning.jpeg')";
+    greeting.textContent = "Good morning,";
+  } else if (hour < 18) {
+    // afternoon
+    document.body.style.backgroundImage = "url('images/afternoon.jpeg')";
+    greeting.textContent = "Good afternoon,";
+  } else {
+    // evening
+    document.body.style.backgroundImage = "url('images/night.jpeg')";
+    greeting.textContent = "Good evening,";
+  }
+}
+
+backgroundGreeting();
+
+
